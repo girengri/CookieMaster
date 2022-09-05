cd14c53e1d84366a46ae91fa06f11d16cd15d592
Giovany <giorengri.96@yahoo.com>
2022-09-05 00:53:35 -0500

    Version inicial CookieMaster

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect, useState } from "react";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import { darkTheme, lightTheme, customTheme } from "../themes";
+import Cookies from "js-cookie";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [currentTheme, setCurrentTheme] = useState(lightTheme);
+
+  useEffect(() => {
+    const cookieTheme = Cookies.get("theme") || "light";
+    const selectedTheme =
+      cookieTheme === "light"
+        ? lightTheme
+        : cookieTheme === "dark"
+          ? darkTheme
+          : customTheme;
+
+    setCurrentTheme(selectedTheme);
+  }, []);
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
